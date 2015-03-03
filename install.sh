4291cd48a5bcd5c2c8115bbc09631435e91f7552
Kevin Gustavson <kdgustavson@gmail.com>
2015-03-03 11:10:21 -0600

    Basic install script.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ln -sfhv "~/.dotfiles/runcom/aliases" "~/.aliases"
+ln -sfhv "~/.dotfiles/runcom/bash_profile" "~/.bash_profile"
+ln -sfhv "~/.dotfiles/runcom/exports" "~/.exports"
+ln -sfhv "~/.dotfiles/runcom/functions" "~/.functions"
+ln -sfhv "~/.dotfiles/runcom/inputrc" "~/.inputrc"
+ln -sfhv "~/.dotfiles/runcom/prompt" "~/.prompt"
+
+ln -sfhv "~/.dotfiles/git/gitconfig" "~/.gitconfig"
+ln -sfhv "~/.dotfiles/git/gitignore" "~/.gitignore"
+
